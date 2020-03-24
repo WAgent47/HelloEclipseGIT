deee95ce1d69c9d33a0e8733642ff3482a041419
WAgent47 <aljaz96.kunst@gmail.com>
2020-03-24 19:51:15 +0100

    push 2

---
+++ src/HelloEclipseGIT.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author Aljaž
+ *
+ */
+public class HelloEclipseGIT {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+	}
+
+}
